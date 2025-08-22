ec6218eedecbf11fe94f75979d5bbd26a23ede4b
Sukmadi <54486758+MathematicGuy@users.noreply.github.com>
2025-08-22 15:50:41 +0700

    thing

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ async def generate_saved_endpoint(
     global rag
     if rag is None:
         raise HTTPException(status_code=503, detail="RAGMCQ not ready on server.")
-
+	
     try:
         mcqs = rag.generate_from_qdrant(
             filename=qdrant_filename,
